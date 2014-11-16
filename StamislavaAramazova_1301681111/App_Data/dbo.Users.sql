a3d7da5b4e9b96f6ad74b2f06e975824d451208b
Stanisslava <stanislava_aramazova@abv.bg>
2014-11-17 00:14:33 +0200

    My really final commit

---
+++ StamislavaAramazova_1301681111/App_Data/dbo.Users.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Users] (
+    [id]       INT        NOT NULL,
+    [username] NCHAR (10) NOT NULL,
+    [password] NCHAR (10) NOT NULL,
+    [email]    NCHAR (20) NOT NULL,
+    CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED ([id] ASC)
+);
+
---
+++ StamislavaAramazova_1301681111/index.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace StamislavaAramazova1301681111
+{
+    public partial class index : System.Web.UI.Page
+    {
+        
+        
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void bttnAdd_Click(object sender, EventArgs e)
+        {
+            SqlConnection connection = new SqlConnection();
+            connection.ConnectionString =
+            "Data Source=Aramazovi;" +
+            "Initial Catalog=User;" +
+            "Integrated Security=True;";
+            connection.Open();
+
+            using (SqlCommand command = connection.CreateCommand())
+            {
+                command.CommandText = "INSERT INTO Users(id,username,password,email) VALUES(@id,@username,@password,@email)";
+
+                command.Parameters.AddWithValue("@id", txt_id.Text);
+                command.Parameters.AddWithValue("@username", txt_user.Text);
+                command.Parameters.AddWithValue("@password", txt_pass.Text);
+                command.Parameters.AddWithValue("@email", txt_email.Text);
+
+                command.ExecuteNonQuery();
+                GridView1.DataBind();
+                connection.Close();
+            }
+        }
+    }
+}
