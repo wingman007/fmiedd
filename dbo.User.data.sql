740280ef606bdd824204304f66472369a8fdf8f0
SimonaIvanova <simonaivanova23@gmail.com>
2014-11-17 21:12:41 +0200

    late but done

---
+++ dbo.User.data.sql
@@ -0,0 +1,3 @@
+﻿SET IDENTITY_INSERT [dbo].[User] ON
+INSERT INTO [dbo].[User] ([Id], [Username], [Password], [Email]) VALUES (1, 'test', '362df','some@abv.bg')
+SET IDENTITY_INSERT [dbo].[User] OFF
