6b57abfdcbee8b4569b2913d9d8fe08740149775
wingman007 <cheresharov@hotmail.com>
2014-11-18 11:55:10 +0200

    Deleteing All in the root

--- dbo.Table.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[UserData]
-(
-	[Id] INT NOT NULL PRIMARY KEY, 
-    [Username] NVARCHAR(50) NULL, 
-    [Password] NCHAR(10) NULL, 
-    [Identification number] INT NULL, 
-    [Email] NCHAR(10) NULL
-)
--- dbo.User.data.sql
+++
@@ -1,3 +0,0 @@
-﻿SET IDENTITY_INSERT [dbo].[User] ON
-INSERT INTO [dbo].[User] ([Id], [Username], [Password], [Email]) VALUES (1, 'test', '362df','some@abv.bg')
-SET IDENTITY_INSERT [dbo].[User] OFF
