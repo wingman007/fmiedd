2747a99c9db48031518084d79e9e1a6528c05e80
hristi1053 <h.dekova@abv.bg>
2014-11-18 09:28:34 +0200

    first commit

---
+++ 1301681053_HristinaDekova/LASTtry/Domain/Users.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Users
+    {
+        int id;
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        string userName;
+
+        public string UserName
+        {
+            get { return userName; }
+            set { userName = value; }
+        }
+        string password;
+
+        public string Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+        string email;
+
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+
+        public override string ToString()
+        {
+            return UserName + " " + Password + " "+ Email ;
+        }
+    }
+}
