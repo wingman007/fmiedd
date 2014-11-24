16dbd0389e4e1916c7ceb31d6878285ae746420b
atravlev <alexandertravlev@gmail.com>
2014-11-24 23:43:27 +0200

    ThirdCommit

--- CRUD/Site12/Update.aspx.designer.cs
+++ CRUD/Site12/Update.aspx.designer.cs
@@ -29,5 +29,14 @@ namespace Site12 {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.GridView gridUsers;
+        
+        /// <summary>
+        /// SqlDataSource1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SqlDataSource SqlDataSource1;
     }
 }
