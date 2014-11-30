10dd55c6aae5d3364c645a50cf488d1111b964e5
pavel945 <pavelski1994@gmail.com>
2014-11-30 21:09:21 +0200

    login form

--- 1301681031_Pavel_Bogdanov/CRUD/CRUD/Program.cs
+++ 1301681031_Pavel_Bogdanov/CRUD/CRUD/Program.cs
@@ -8,6 +8,8 @@ namespace CRUD
 {
     class Program
     {
+        static bool user_kind = false;
+        static bool flag = false;
         static int id = 0;
         static OleDbDataReader dr = null;
         static OleDbCommand cmd = new OleDbCommand();
@@ -24,16 +26,68 @@ namespace CRUD
                 {
                     while (dr.Read())
                     {
-
                         Console.WriteLine("ID: " + dr[0].ToString() + "; " + "Username: " + dr[1].ToString() + "; " + "Password: " + dr[2].ToString() + "; " + "email: " + dr[3].ToString());
                         id = Convert.ToInt32(dr[0]);
                     }
-
+                    dr.Close();
+                    cn.Close();
                 }
-                dr.Close();
-
-                cn.Close();
             }
+            catch (Exception e)
+            {
+                cn.Close();
+                Console.WriteLine(e.Message.ToString());
+            }
+        }
+        public static void LogIn(OleDbConnection cn, OleDbCommand cmd, OleDbDataReader dr)
+        {
+            Console.WriteLine("username= ");
+            string log_name = Console.ReadLine();
+            Console.WriteLine("pass= ");
+            string log_pass = Console.ReadLine();
+            string name = "";
+            string pass = "";
+            try
+            {
+                string q = "select * from roles";
+                cmd.CommandText = q;
+                cn.Open();
+                dr = cmd.ExecuteReader();
+                if (dr.HasRows)
+                {
+                    while (dr.Read())
+                    {
+                         name = dr[1].ToString();
+                         pass = dr[2].ToString();
+                         if (log_name == name && log_pass == pass)
+                         {
+                             flag = true;
+                         }
+                         else
+                         {
+                             name = "";
+                             pass = "";
+                         }
+                        }
+                        if (flag == true)
+                        {
+                            Console.WriteLine("You are logged as "+ log_pass);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Wrong username or password, please try again.");
+                            Console.WriteLine();
+                        }
+                    }
+                if (log_pass == "admin")
+                {
+                    user_kind = true;
+                }
+                else
+                { user_kind = false; }
+                dr.Close();
+                cn.Close();
+                }
             catch (Exception e)
             {
                 cn.Close();
@@ -57,7 +111,6 @@ namespace CRUD
         }
         public static void Create(OleDbConnection cn, OleDbCommand cmd)
         {
-
             Console.WriteLine("-------------------------------------");
             Console.WriteLine("Create a record in database: ");
             Reader(cn, cmd, dr);
@@ -76,7 +129,6 @@ namespace CRUD
         }
         public static void Update(OleDbConnection cn, OleDbCommand cmd)
         {
-
             Console.WriteLine("-------------------------------------");
             Console.WriteLine("Update a record in database: ");
             Reader(cn, cmd, dr);
@@ -89,13 +141,10 @@ namespace CRUD
             string password2 = (Console.ReadLine());
             Console.WriteLine("E-mail: ");
             string email = (Console.ReadLine());
-
             string u = "update info set username ='" + username + "',password2 ='" + password2 + "',email ='" + email + "'where id=" + id;
             Cud(u, cn, cmd);
             Console.WriteLine();
             Reader(cn, cmd, dr);
-
-
         }
         public static void Delete(OleDbConnection cn, OleDbCommand cmd)
         {
@@ -108,17 +157,23 @@ namespace CRUD
             string p = "delete * from info where id=" + id_del;
             Cud(p, cn, cmd);
             Reader(cn, cmd, dr);
-
         }
         static void Main(string[] args)
         {
-            cn.ConnectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=c:\users\pavel\documents\visual studio 2012\Projects\Cruid\Cruid\Database1.accdb;Persist Security Info=True";
+            cn.ConnectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\Pavel\Source\Repos\fmiedd\1301681031_Pavel_Bogdanov\CRUD\CRUD\Database1.accdb;Persist Security Info=True";
             cmd.Connection = cn;
-
-            Create(cn, cmd);
-            Update(cn, cmd);
-            Delete(cn, cmd);
-
+            while (flag == false)
+            { LogIn(cn, cmd, dr); }
+            if (user_kind == true)
+            {
+                Create(cn, cmd);
+                Update(cn, cmd);
+                Delete(cn, cmd);
+            }
+            else
+            {
+                Reader(cn, cmd, dr);
+            }
             Console.ReadKey();
         }
     }
