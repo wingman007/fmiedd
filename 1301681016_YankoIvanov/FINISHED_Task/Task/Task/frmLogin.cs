b48a60bee5e7269e8ea10c3f51cdce577535aa27
Yaneka <qnek2101@abv.bg>
2014-11-30 19:52:27 +0200

    LastWEEKYankoIvanov

---
+++ 1301681016_YankoIvanov/FINISHED_Task/Task/Task/frmLogin.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Task.Source;
+
+namespace Task
+{
+    public partial class frmLogin : Form
+    {
+        private bool message_show;
+        public static string loggedUser;
+
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            frmReg frmReg = new frmReg();
+            frmReg.ShowDialog();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            message_show = Auth.ConnectUser(tbUsername.Text,tbPassword.Text);
+
+            if (message_show==true)
+            {
+                loggedUser = tbUsername.Text;
+                frmMain frmMain = new frmMain();
+                frmMain.Show();
+                this.Hide();
+            }
+            else MessageBox.Show("Username or password incorrect!");
+        }
+    }
+}
---
+++ 1301681016_YankoIvanov/FINISHED_Task/Task/Task/frmUserInfo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Task.Source;
+using System.Data.SqlServerCe;
+
+namespace Task
+{
+    public partial class frmUserInfo : Form
+    {
+        public frmUserInfo()
+        {
+            InitializeComponent();
+
+            LoggedUserCheck();
+        }
+        private void LoggedUserCheck()
+        {
+            SqlCeConnection sqlConnection = new SqlCeConnection();
+            sqlConnection.ConnectionString = Auth.ConnectionDBString;
+
+            SqlCeCommand sqlCommand = new SqlCeCommand();
+            sqlCommand.CommandType = System.Data.CommandType.Text;
+            sqlCommand.CommandText = "select * from Users where username='" + frmLogin.loggedUser + "'";
+            sqlCommand.Connection = sqlConnection;
+            sqlConnection.Open();
+            SqlCeDataReader rd = sqlCommand.ExecuteReader();
+            while (rd.Read())
+            {
+                tbUsername.Text = rd.GetValue(0).ToString();
+                tbNames.Text = rd.GetValue(1).ToString();
+                tbPassword.Text = rd.GetValue(2).ToString();
+                tbAdmin.Text = rd.GetValue(3).ToString();
+            }
+            sqlConnection.Close();
+        }
+    }
+}
