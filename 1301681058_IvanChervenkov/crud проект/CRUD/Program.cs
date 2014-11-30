9d1f668e29bb7ab189359f795a937a58d13222df
Ivan <tuaLight@gmail.com>
2014-11-30 20:23:08 +0200

    Проектът, който е за 10-та седмица

--- 1301681058_IvanChervenkov/crud проект/CRUD/Program.cs
+++ 1301681058_IvanChervenkov/crud проект/CRUD/Program.cs
@@ -8,11 +8,36 @@ namespace CRUD
 {
     class Program
     {
+        static void Login()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Username :");
+            string username = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Password :");
+            string pass = Convert.ToString(Console.ReadLine());
+
+            Authentication.AuthenticateUser(username, pass);
+
+            if (Authentication.LoggedUser == null)
+            {
+                Login();
+            }
+        }
         static void Main(string[] args)
+
         {
             Menu menu = new Menu();
-            menu.Show();
-         
+            Login();
+            UserRepository.GetRole(Authentication.LoggedUser);
+            if (Authentication.LoggedUser.Role == "admin")
+            {
+                menu.Show();
+            }
+           if (Authentication.LoggedUser.Role=="member")
+           {
+           menu.Member();
+           }
         }
     }
 }
--- 1301681058_IvanChervenkov/crud проект/CRUD/UserRepository.cs
+++ 1301681058_IvanChervenkov/crud проект/CRUD/UserRepository.cs
@@ -10,17 +10,81 @@ namespace CRUD
 {
     class UserRepository
     {
-        private readonly string dataString = "Server=VANKO-PC\\VANKO;Database=users;Trusted_Connection=True;";
-        private SqlConnection conn = null;
+        private static readonly string dataString = "Server=VANKO-PC\\VANKO;Database=users;Trusted_Connection=True;";
+        private static SqlConnection conn = null;
         public UserRepository()
         {            
-            this.conn = new SqlConnection() ; 
+           conn = new SqlConnection() ; 
+        }
+
+        static public User GetByUsernameAndPassword(string username, string password)
+        {
+            conn = new SqlConnection();
+
+            User loguser = new User();
+
+            SqlCommand cmd = conn.CreateCommand();
+            conn.ConnectionString = dataString;
+            cmd.CommandText = @"
+SELECT
+  ID,
+  username,
+  full_name,
+  email,
+  pass
+FROM
+  users
+WHERE
+  username = @username
+AND
+  pass = @pass
+";
+            SqlParameter param = cmd.CreateParameter();
+
+            param = cmd.CreateParameter();
+            param.ParameterName = "@username";
+            param.Value = username;
+            cmd.Parameters.Add(param);
+
+            param = cmd.CreateParameter();
+            param.ParameterName = "@pass";
+            param.Value = password;
+            cmd.Parameters.Add(param);
+
+            try
+            {
+                conn.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    loguser.ID = Convert.ToInt32(reader["ID"]);
+                    loguser.Username = Convert.ToString(reader["username"]);
+                    loguser.Full_Name = Convert.ToString(reader["full_name"]);
+                    loguser.Email = Convert.ToString(reader["email"]);
+                };
+
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error"+ex.Message);
+
+            }
+            finally
+            {
+                conn.Close();
+            }
+            if (loguser.Username == null)
+            {
+                return null;
+            }
+            else return loguser;
         }
       //Добавяме
         public void Add(User item)
         {
-            SqlCommand cmd = this.conn.CreateCommand();
-            this.conn.ConnectionString = dataString;
+            SqlCommand cmd = conn.CreateCommand();
+            conn.ConnectionString = dataString;
             cmd.CommandText = @"
 INSERT INTO users(
   username,
@@ -57,7 +121,7 @@ VALUES (
 
             try
             {
-                this.conn.Open();
+                conn.Open();
                 if(cmd.ExecuteNonQuery()>0)
                   Console.WriteLine("User saved successfully.");
             }
@@ -67,7 +131,7 @@ VALUES (
             }
             finally
             {
-                this.conn.Close();
+               conn.Close();
             }
         }
 
@@ -76,9 +140,9 @@ VALUES (
         {
             List<User> result = new List<User>();
 
-            this.conn.ConnectionString =dataString;
-            SqlCommand cmd = this.conn.CreateCommand();
-            cmd.Connection = this.conn;
+            conn.ConnectionString =dataString;
+            SqlCommand cmd = conn.CreateCommand();
+            cmd.Connection = conn;
             cmd.CommandText = @"
 SELECT
   ID,
@@ -92,7 +156,7 @@ FROM
 
             try
             {
-                this.conn.Open();
+                conn.Open();
                 IDataReader reader = cmd.ExecuteReader();
                 while (reader.Read())
                     result.Add(new User()
@@ -113,16 +177,56 @@ FROM
             }
             finally
             {
-                this.conn.Close();
+               conn.Close();
             }
             return result;
         }
+        //Роли
+        static public void GetRole(User user)
+        {
+            conn = new SqlConnection();
+            conn.ConnectionString = dataString;
+            SqlCommand cmd = conn.CreateCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = @"
+SELECT
+  role
+FROM
+  roles
+WHERE 
+  ID = @ID
+";
+            SqlParameter param = cmd.CreateParameter();
+            param.ParameterName = "@ID";
+            param.Value = user.ID;
+            cmd.Parameters.Add(param);
 
+            try
+            {
+                conn.Open();
+                IDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                   {
+                       user.Role = Convert.ToString(reader["role"]);
+                    };
+
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.Write(ex.Message,
+                     "Database operation Failed " + ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
         //Обновяване
         public void Update(User item)
         {
-            SqlCommand cmd = this.conn.CreateCommand();
-            this.conn.ConnectionString = dataString;   
+            SqlCommand cmd = conn.CreateCommand();
+            conn.ConnectionString = dataString;   
                     cmd.CommandText = @"
 UPDATE users SET
   pass= @pass,
@@ -153,7 +257,7 @@ WHERE
                 
                 try
                 {
-                    this.conn.Open();
+                    conn.Open();
                     if (cmd.ExecuteNonQuery() > 0)
                         Console.WriteLine("User edit successfully.");
                     else Console.WriteLine("This user doesn't exist.");
@@ -164,7 +268,7 @@ WHERE
                 }
                 finally
                 {
-                    this.conn.Close();
+                    conn.Close();
                 }
         
         }
@@ -172,7 +276,7 @@ WHERE
         //Триене
         public void Delete(string username)
         {
-            SqlCommand cmd = this.conn.CreateCommand();
+            SqlCommand cmd = conn.CreateCommand();
             conn.ConnectionString = dataString;
             cmd.CommandText = @"
 DELETE FROM users
@@ -187,7 +291,7 @@ WHERE
 
             try
             {
-                this.conn.Open();
+                conn.Open();
                 if (cmd.ExecuteNonQuery() == 0) Console.WriteLine("Delete failed!");
                 else Console.WriteLine("User deleted successfully.");
             }
@@ -197,8 +301,9 @@ WHERE
             }
             finally
             {
-                this.conn.Close();
+                conn.Close();
             }
+
         }
     }
 }
